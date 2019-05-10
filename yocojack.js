eb1ea741fe5d552d468dc25583e6f24fdd4bbead
Nahashon Njenga <nahaseannjenga@gmail.com>
2019-05-10 09:30:36 +0300

    Added cardValue function

--- yocojack.js
+++ yocojack.js
@@ -28,3 +28,26 @@ function multiple  (input) {
             return 1
     }
 }
+
+/**
+ * This function returns the value of the card number.
+ * 2-9 are each worth the value on the card number
+ * K, Q, J and 10 are each worth 10 points
+ * A is worth 11 points
+ * @param input
+ * @returns {number}
+ */
+ function cardNumberValue ( input) {
+     //first check if the card is either of the values below and return
+    // either 10 or 11 depending on the fulfilled condition.
+    //This also helps filter out string values that cannot be parsed to numbers.
+    if (input === "10" || input === "J" || input === "Q" || input === "K") {
+        return 10
+    }
+    if (input === "A") {
+        return 11
+    }
+    //convert the remaining string input into a number. This means each card
+    // holds a value of the card number
+    return Number.parseInt(input)
+}
